6f23b8d14ce38c11fe4d61699195da8d0a50f274
Skarstein-Hatlen <kjetil.skarstein-hatlen@hotmail.no>
2022-12-08 15:42:58 +0100

    Major update to code, some styling and minor html

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Kjetil_Skarstein-Hatlen_sp1
+
+Need to use font-awesome for bicycle icon as bootstrap does not have a bicycle.
--- Web Application/wdt_app.js
+++ Web Application/wdt_app.js
@@ -1,217 +1,317 @@
-//Get staff users from api
+let staffMembers = [];
+
 $(document).ready(function() {
-        $.ajax({
-            url: 'https://randomuser.me/api/?results=5&inc=picture,name,email',
-            dataType: 'json',
-            success: function staffUserGet(data) {
-                console.log(data)
-                var userData = data.results
-                    $("#user1").html(
-                    "<td>"+"<img src='"+userData[0].picture.thumbnail+"'>"+"</img>"+"</td>"+
-                    "<td>"+userData[0].name.first+"</td>"+
-                    "<td>"+userData[0].name.last+"</td>"+
-                    "<td>"+userData[0].email+"</td>"+
-                    "<td>"+"IN"+"</td>"+
-                    "<td>"+""+"</td>"+
-                    "<td>"+""+"</td>"+
-                    "<td>"+""+"</td>"
-                    )
-                    $("#user2").html(
-                    "<td>"+"<img src='"+userData[1].picture.thumbnail+"'>"+"</img>"+"</td>"+
-                    "<td>"+userData[1].name.first+"</td>"+
-                    "<td>"+userData[1].name.last+"</td>"+
-                    "<td>"+userData[1].email+"</td>"+
-                    "<td>"+"IN"+"</td>"+
-                    "<td>"+""+"</td>"+
-                    "<td>"+""+"</td>"+
-                    "<td>"+""+"</td>"
-                    )
-                    $("#user3").html(
-                    "<td>"+"<img src='"+userData[2].picture.thumbnail+"'>"+"</img>"+"</td>"+
-                    "<td>"+userData[2].name.first+"</td>"+
-                    "<td>"+userData[2].name.last+"</td>"+
-                    "<td>"+userData[2].email+"</td>"+
-                    "<td>"+"IN"+"</td>"+
-                    "<td>"+""+"</td>"+
-                    "<td>"+""+"</td>"+
-                    "<td>"+""+"</td>"
-                    )
-                    $("#user4").html(
-                    "<td>"+"<img src='"+userData[3].picture.thumbnail+"'>"+"</img>"+"</td>"+
-                    "<td>"+userData[3].name.first+"</td>"+
-                    "<td>"+userData[3].name.last+"</td>"+
-                    "<td>"+userData[3].email+"</td>"+
-                    "<td>"+"IN"+"</td>"+
-                    "<td>"+""+"</td>"+
-                    "<td>"+""+"</td>"+
-                    "<td>"+""+"</td>"
-                    )
-                    $("#user5").html(
-                    "<td>"+"<img src='"+userData[4].picture.thumbnail+"'>"+"</img>"+"</td>"+
-                    "<td>"+userData[4].name.first+"</td>"+
-                    "<td>"+userData[4].name.last+"</td>"+
-                    "<td>"+userData[4].email+"</td>"+
-                    "<td>"+"IN"+"</td>"+
-                    "<td>"+""+"</td>"+
-                    "<td>"+""+"</td>"+
-                    "<td>"+""+"</td>"
-                    )
-                
-            }
-        });
-
-})
-
-
-
-//Staff Table
-$(document).ready(function() {
-
+    // Get staff members
+    staffUserGet().then(staff => staffMembers = staff);
 
     //Table selected
-    $('#staffTable tbody').on('click', 'tr', function () {
-        if ($(this).hasClass('selected')) {
-            $(this).removeClass('selected');
-        } else {
-            $("tr.selected").removeClass('selected');
-            $(this).addClass('selected');
-        } 
+    $("#staffTable tbody").on("click", "tr", selectStaff);
 
-        //Staff Out
-        $("#outBtn").on("click", function staffOut(){
-            
-            //functions
-            var userAnswer = parseFloat(prompt("Please provide the absence of the staff member in minutes:"))
+    //Staff Out
+    $("#outBtn").on("click", staffOut);
 
-            
-            //Status Out
-            $("tr.selected:contains('IN')").find("td").eq(4).html("OUT");
+    // Staff in
+    $("#inBtn").on("click", staffIn);
+});
 
-            
-            //Out Time
-            $("tr.selected").find("td").eq(5).html(function currentTime(){
-                const d = new Date();
-                var hour = d.getHours();
-                var minutes = d.getMinutes();
-                hour = checkTime(hour)
-                minutes = checkTime(minutes)
-                var currentTime = hour + ":" + minutes
-                function checkTime(i){
-                    if(i < 10){i = "0" + i}
-                    return i; 
-                }
-                return currentTime
-            });
+function selectStaff() {
+    if ($(this).hasClass("selected")) {
+        $(this).removeClass("selected");
+    } else {
+        $("tr.selected").removeClass("selected");
+        $(this).addClass("selected");
+    }
+}
 
-            
-            //Duration
-            $("tr.selected").find("td").eq(6).html(function duration(){
-                function timeConvert(num){ 
-                    var hours = Math.floor(num / 60); 
-                    var minutes = num % 60;
-                    if(userAnswer < 60){
-                        return minutes + "min"
-                    } else{
-                        return hours + "hr" + " " + minutes + "min";
-                    }
-                }
-                while(true){
-                    if(!isNaN(userAnswer)){
-                        return timeConvert(userAnswer)
-                    } else{
-                        userAnswer = parseFloat(prompt("please provide the minutes in numbers"))
-                    }   
-                }
-            })
+function staffOut(){
+    let selected = $("tr.selected");
+    let userId = parseFloat(selected[0].id.substring(4));
 
-            //Expected return Time
-            $("tr.selected").find("td").eq(7).html(function expectedReturn(){
-                function timeConvert(num){
-                    const d = new Date();
-                    var hour = d.getHours();
-                    var minutes = d.getMinutes();
-                    var cHours = hour + Math.floor(num / 60);
-                    var cMinutes = (minutes + num) % 60;
-                    console.log (minutes + num)
-                    if((minutes + num) > 60 && hour == cHours){
-                        cHours = cHours + Math.floor((minutes + num) / 60)
-                    }
-                    var expReturn = checkTime(cHours) + ":" + checkTime(cMinutes)
-                    return expReturn;
+    //functions
+    let userAnswer = parseFloat(prompt("Please provide the absence of the staff member in minutes:"));
+    let staffMember = staffMembers.find(c => c.id === userId);
 
-                };
-                function checkTime(i){
-                    if(i < 10){i = "0" + i}
-                    return i; 
-                }
+    //Expected return Time
+    let expectedBack = expectedReturn(userAnswer);
+    staffMember.out(expectedBack);
+    selected.find("td").eq(7).html(expectedBack)
+
+    //Status Out
+    $("tr.selected:contains('IN')").find("td").eq(4).html("OUT");
+
+    //Out Time
+    selected.find("td").eq(5).html(currentTime());
+
+    //Duration
+    selected.find("td").eq(6).html(function duration(){
+        function timeConvert(num){
+            var hours = Math.floor(num / 60);
+            var minutes = num % 60;
+            if(userAnswer < 60){
+                return minutes + "min"
+            } else{
+                return hours + "hr" + " " + minutes + "min";
+            }
+        }
+        while(true){
+            if(!isNaN(userAnswer)){
                 return timeConvert(userAnswer)
-            })
+            } else{
+                userAnswer = prompt("Please provide the minutes in numbers");
+            }
+        }
+    })
 
-            //Toast if staff remeber is late
-        })
+    setTimeout(() => {
+        let staff = staffMembers.find(c => c.id === userId);
 
+        console.log(`Staff ${staff.id} has returned: ${!staff.isOut}`);
+        if(staff.isOut) {
+            staffMemberIsLate(staff, userAnswer);
+        }
+    }, userAnswer * 60 * 1000); // popup
+}
 
-        //Staff In
-        $("#inBtn").on("click", function staffIn(){
-            $("tr.selected:contains('OUT')").find("td").eq(4).html("IN")
-            $("tr.selected").find("td").eq(5).html("")
-            $("tr.selected").find("td").eq(6).html("")
-            $("tr.selected").find("td").eq(7).html("")
-        })
+function staffIn(){
+    let selected = $("tr.selected");
+    let userId = parseFloat(selected[0].id.substring(4));
+    let staffMember = staffMembers.find(c => c.id === userId);
+    staffMember.in();
+
+    $("tr.selected:contains('OUT')").find("td").eq(4).html("IN")
+    selected.find("td").eq(5).html("")
+    selected.find("td").eq(6).html("")
+    selected.find("td").eq(7).html("")
+}
+
+//Toast if staff remeber is late
+function staffMemberIsLate(staffMember){
+    $("#toastImg").attr("src", staffMember.avatar);
+    $("#toastSmall").html(overdueMin + "min overdue");
+    $("#toastName").html(staffMember.name.first + " " +staffMember.name.last);
+    $("#toastMessage").html(`${staffMember.displayName} is late, we expected ${staffMember.name.last} back ${staffMember.expectedReturn}`);
+    $("#liveToast").removeClass("hide").addClass("show");
+    $("#closeBtn").on("click", function closeToast(){
+        $("#liveToast").removeClass("show").addClass("hide")
     });
-})
+};
 
+let overdueMin = 0;
+function overdue() {
+    if ($("#liveToast").hasClass("show")) {
+    overdueMin += 1;
+    }
+} 
+setInterval(overdue, 1000);
+console.log(overdueMin)
 
-//Schedule delivery
+//Schedule delivery and add to delivery board
 $(document).ready(function(){
 
-    //Add button
+    //Add button for adding delivery to board
     $("#addBtn").on("click", function addDelivery(){
-        $("#tBodyBoard").html("<tr>" +
+        let deliveryInfo = "<tr>" +
         "<td>" + $("#vehicle").val() + "</td>" +
         "<td>" + $("#inputFName").val() + "</td>" +
         "<td>" + $("#inputLName").val() + "</td>" +
         "<td>" + $("#phone").val() + "</td>" +
         "<td>" + $("#inputAdress").val() + "</td>" +
-        "<td>" + $("#inputAdress") + "</td>" +
-        "</tr>");
-    }) 
+        "<td>" + $("#returnTime").val() + "</td>" +
+        "</tr>";
+        $("#tBodyBoard").append(deliveryInfo);
+    });
 
     //Remove selected delivery with clear Button
-    $('#tBodyBoard').on('click', 'tr', function removeDelivery() {
-        if ($(this).hasClass('selected')) {
-            $(this).removeClass('selected');
+    $("#tBodyBoard").on("click", "tr", function removeDelivery() {
+        if ($(this).hasClass("selected")) {
+            $(this).removeClass("selected");
         } else {
-            $("tr.selected").removeClass('selected');
-            $(this).addClass('selected');
-        } 
+            $("tr.selected").removeClass("selected");
+            $(this).addClass("selected");
+        }
         $("#clearBtn").on("click", function clearButton(){
             $("tr.selected").html("")
         })
     })
+
+    //Validate Delivery function
+
+    //Vehicle
+    $("#vehicle").on("change", function(){
+        let value = $(this).eq(0).val().toLowerCase();
+        if(value !== "car") {
+            $(this).val("");
+            alert("Please try again and enter the either a car or a motorcycle");
+        } else {
+            return $(this).eq(0).val()
+        }        
+    })
+
+    //First name
+    $("#inputFName").on("change", function(){
+        let regexLetters = /^[a-zA-Z]+$/;
+        if(!regexLetters.test($(this).val())) {
+            $(this).val("");
+            alert("Please try again and enter a first name with letters only");
+        } else {
+            return $(this).eq(0).val()
+        }        
+    })
+
+    //Last Name
+    $("#inputLName").on("change", function(){
+        let regexLetters = /^[a-zA-Z]+$/;
+        if(!regexLetters.test($(this).val())) {
+            $(this).val("");
+            alert("Please try again and enter a last name with letters only");
+        } else {
+            return $(this).eq(0).val()
+        }        
+    })
+
+    //Phone number
+    $("#phone").on("change", function(){
+        let regexPhone = /^\+?[0-9]{7,}([0-9-]*[0-9])?$/;
+        if(!regexPhone.test($(this).val())) {
+            $(this).val("");
+            alert("Please provide a valid phone number of at least 7 numbers");
+        } else {
+            return $(this).eq(0).val()
+        }        
+    })
+
+    //Adress
+    $("#inputAdress").on("change", function(){
+        let regexAdress = /^(?=.*\d)(?=.*[a-zA-Z]).+$/;
+        if(!regexAdress.test($(this).val())) {
+            $(this).val("");
+            alert("Please enter a valid adress with both street and number");
+        } else {
+            return $(this).eq(0).val()
+        }        
+    })
+
+    //Return Time
+    $("#returnTime").on("change", function(){
+        const currentTime = new Date()
+        let enteredTime = new Date();
+        let regexReturn = /^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
+        enteredTime.setHours($(this).val().split(":")[0]);
+        enteredTime.setMinutes($(this).val().split(":")[1]);
+        if(!regexReturn.test($(this).val())) {
+            $(this).val("");
+            alert("Please enter a valid time format");
+        } else if(enteredTime > currentTime) {
+            $(this).val("");
+            alert("Please enter a valid time in format the present")
+        }
+        else {
+            return $(this).eq(0).val()
+        }        
+    })
+
+    //Delivery Driver is late function
 })
 
 
+//--------------------------------------------------------------------//
 
+/**
+ * Staff section
+ */
+class StaffMember {
+    constructor(user, id) {
+        this.id = id;
+        this.email = user.email;
+        this.name = {...user.name}
+        this.displayName = `${this.name.first} ${this.name.last}`
+        this.avatar = user.picture.thumbnail;
+        this.isOut = false;
+        this.outTime = '';
+        this.expectedReturn = '';
+    }
 
+    out(expectedReturn){
+        this.isOut = true;
+        this.outTime = currentTime();
+        this.expectedReturn = expectedReturn;
+    }
 
+    in(){
+        this.isOut = false;
+        this.outTime = '';
+    }
+}
 
+/**
+ * retrieve users, map them to staff members and add them to the view.
+ * @returns {Promise<StaffMember[]>}
+ */
+async function staffUserGet(){
+    let staffMembers = await fetch('https://randomuser.me/api/?results=5&inc=picture,name,email')
+        .then(result => result.json())
+        .then(data => data.results.map((user, i) => new StaffMember(user, i+1)));
 
+    for(const staffMember of staffMembers) {
+        const targetEl = `#user${staffMember.id}`;
 
+        $(targetEl).html(
+            "<td>" + "<img alt='avatar' src='" + staffMember.avatar + "'/></td>" +
+            "<td>" + staffMember.name.first + "</td>" +
+            "<td>" + staffMember.name.last + "</td>" +
+            "<td>" + staffMember.email + "</td>" +
+            "<td>" + "IN" + "</td>" +
+            "<td>" + "" + "</td>" +
+            "<td>" + "" + "</td>" +
+            "<td>" + "" + "</td>"
+        );
+    }
 
+    return staffMembers;
+}
 
+/**
+ * returns the current time
+ * @returns {string}
+ */
+function currentTime(){
+    const d = new Date();
+    let hour = d.getHours();
+    let minutes = d.getMinutes();
+    hour = checkTime(hour);
+    minutes = checkTime(minutes);
+    return hour + ":" + minutes;
+}
 
+/**
+ * adds leading zeros to the 24hr clock
+ * @returns {string}
+ */
+function checkTime(i){
+    if(i < 10){i = "0" + i}
+    return i;
+}
 
+/**
+ * returns expected return time of staff members
+ * @returns {string}
+ */
+function expectedReturn(userAnswer){
+    function timeConvert(num){
+        const d = new Date();
+        var hour = d.getHours();
+        var minutes = d.getMinutes();
+        var cHours = hour + Math.floor(num / 60);
+        var cMinutes = (minutes + num) % 60;
 
+        if((minutes + num) > 60 && hour == cHours){
+            cHours = cHours + Math.floor((minutes + num) / 60)
+        }
+        return checkTime(cHours) + ":" + checkTime(cMinutes)
+    }
 
-
-
-
-
-
-
-
-
+    return timeConvert(userAnswer)
+}
 
 //Digital Clock Function
 $(document).ready(function() {
@@ -230,10 +330,5 @@ $(document).ready(function() {
         seconds = checkTime(seconds)
         var currentTime = day + "-" + month + "-" + year + " " + hour + ":" + minutes + ":" + seconds;
         $("#dateTime").html(currentTime);
-        
     }, 1000);
-    function checkTime(i){
-        if(i < 10){i = "0" + i}
-        return i; 
-    }
 })
